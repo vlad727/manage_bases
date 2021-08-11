05bb98c8bed02c3c95baf34184f12551585cd45f
vlad727 <kyz727@mail.ru>
2021-08-11 07:30:44 +0300

    release v1.1

--- manage_bases.sh
+++ manage_bases.sh
@@ -18,11 +18,13 @@ if test $# -eq 0; then
 	echo " bash manage_bases.sh empty_base         создать пустую базу для будущего восстановления"
 	echo " bash manage_bases.sh drop_base          удалить базу данных"
 	echo " bash manage_bases.sh restore_base       восстановить базу данных из бэкапа"
+	echo " bash manage_bases.sh rename_base        переименовать базу данных"
 fi
 #===================================================================================================================
 case "$1" in
 "show_databases")
-	 psql -U postgres -c '\l';;
+	psql -U postgres -c '\l'
+;;
 
 "create_copy")
 	echo "Input source base name:"
@@ -31,35 +33,41 @@ case "$1" in
 				read dstbase 
 					echo "Source base name  $srcbase" 
 						echo "Destination base name $dstbase" 
-							psql -U postgres -c "CREATE DATABASE $dstbase TEMPLATE $srcbase;";;
+							psql -U postgres -c "CREATE DATABASE $dstbase TEMPLATE $srcbase;"
+;;
 
 "show_backups")
 	echo "Размер | месяц | число | время создания бэкапа | файл бэкапа "
-		ls -lh  /backup_disk/backups/ | awk '{print $5 "      " $6 "      " $7 "      " $8 "                  " $9}';;
+		ls -lh  /backup_disk/backups/ | awk '{print $5 "      " $6 "      " $7 "      " $8 "                  " $9}'
+;;
 
 "unpack_base")
 	echo "Input file source name: "
                 read srcfile
 			echo "Unpack process may take for 15 min"
 				unpigz /backup_disk/backups/$srcfile
-					echo "File is unpacked!!! Run again "show_backups" to get new file name" ;;
+					echo "File is unpacked!!! Run again "show_backups" to get new file name" 
+;;
 
 "empty_base")
 	echo "Input name for new empty database: "
 		read new_database
 			psql -U  postgres -c "CREATE DATABASE -T template0 $new_database;"
-				echo "Empty base has been created with name $new_database" ;;
+				echo "Empty base has been created with name $new_database" 
+;;
 
 "drop_base")
         echo "Input name database which one do you want to drop: "
 		read drop_name
 			psql -U postgres -c "DROP DATABASE $drop_name;"
-				echo "Base with name $drop_name has been dropped" ;;
+				echo "Base with name $drop_name has been dropped"
+;;
 
 "restore_base")
 	echo "Input backup file name: "
 		read file_name
 			echo "Input empty base name: "
 				read empty_base_name
-					psql -U postgres $empty_base_name < /backup_disk/backups/$file_name ;;
+					psql -U postgres $empty_base_name < /backup_disk/backups/$file_name
+;;
 esac
